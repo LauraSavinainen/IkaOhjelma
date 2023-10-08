53d6849a4be74a6ad27de00c9cf901fa5171374a
LauraSavinainen <146864616+LauraSavinainen@users.noreply.github.com>
2023-10-08 22:09:21 +0300

    ver 3
    Tehtävänanto 5.1 mopo ja kevari

--- src/Ikaohjelma.java
+++ src/Ikaohjelma.java
@@ -2,12 +2,19 @@
 public class Ikaohjelma {
 
 	public static void main(String[] args) {
-		int ika = 10;
+		int ika = 16;
 		if (ika >= 0 && ika < 18)
 
 		{
 
 		System.out.println("Olet alaikäinen");
+		if (ika >= 15) {
+			System.out.println("Saat ajaa mopoa");
+			
+			if (ika > 15 && ika < 18) {
+				System.out.println("Saat ajaa kevaria");
+			}
+		}
 
 		}
 		
