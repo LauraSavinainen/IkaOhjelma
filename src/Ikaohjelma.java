c9c20f2787adde2a37d962d0ba51741d941730b5
LauraSavinainen <146864616+LauraSavinainen@users.noreply.github.com>
2023-10-08 22:11:29 +0300

    ver 4
    Tehtävänanto 5.2-5.4

--- src/Ikaohjelma.java
+++ src/Ikaohjelma.java
@@ -2,33 +2,40 @@
 public class Ikaohjelma {
 
 	public static void main(String[] args) {
-		int ika = 16;
-		if (ika >= 0 && ika < 18)
+		int ika = 50;
 
-		{
-
-		System.out.println("Olet alaikäinen");
-		if (ika >= 15) {
-			System.out.println("Saat ajaa mopoa");
-			
-			if (ika > 15 && ika < 18) {
-				System.out.println("Saat ajaa kevaria");
+		if (ika >= 0 && ika < 18) {
+			System.out.println("Olet alaikäinen");
+			if (ika >= 15) {
+				System.out.println("Saat ajaa mopoa");
+				
+				if (ika > 15 && ika < 18) {
+					System.out.println("Saat ajaa kevaria");
+				}
 			}
+			
 		}
-
+		else if(ika == 18) {
+			System.out.println("Olet juuri tullut täysi-ikäiseksi ja saat ajaa autoa");
 		}
 		
-		else if  (ika >= 65) {
+		else if(ika == 20 || ika == 30 || ika == 40 || ika == 50 || ika == 60 || ika == 70 || ika == 80 || ika == 90) {
+			System.out.println("Onnea pyöreistä vuosista!");
+		}
+		else if(ika == 100) {
+			System.out.println("Onnea!");
+			System.out.println("Onnea!");
+			System.out.println("Onnea!");
+		}
+		else if(ika >= 65)
+		{
 			System.out.println("Olet eläkeläinen");
 		}
 
-		else
-
-		{
-
-		System.out.println("Olet aikuinen");
-
+		else {
+			System.out.println("Olet aikuinen");
 		}
+		
 
 	}
 
